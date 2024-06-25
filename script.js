70ef0e892bae40d7c1bc5eb4d520e3ab1dd63b4f
Øystein <oystein.koder@gmail.com>
2024-06-25 09:31:58 +0200

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+const nav_list = document.querySelector (".nav_list");
+const variable_button = document.querySelector (".variable_button");
+
+function toggle_dropdown () {
+    nav_list.classList.toggle ("nav_hidden");
+}
+
+function theme_toggle () {
+    console.log ("Theme toggle clicked");
+    
+}
