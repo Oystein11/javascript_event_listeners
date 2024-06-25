89d3b0f3d078ee742fe08312afe7456e71b89b59
Øystein <oystein.koder@gmail.com>
2024-06-25 09:46:27 +0200

    edit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const nav_list = document.querySelector (".nav_list");
 const variable_button = document.querySelector (".variable_button");
+const toggle_color = document.querySelector ("#theme_change");
 
 function toggle_dropdown () {
     nav_list.classList.toggle ("nav_hidden");
@@ -7,5 +8,5 @@ function toggle_dropdown () {
 
 function theme_toggle () {
     console.log ("Theme toggle clicked");
-    
+    toggle_color.classList.toggle ("dark_mode");
 }
